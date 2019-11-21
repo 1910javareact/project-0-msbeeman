72bc215706e9553f1f7c0846389901acdb0bba51
Matthew <msbeeman@github.com>
2019-11-21 13:10:18 -0500

    Added auth-middleware

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express'
 import bodyparser from 'body-parser'
+import { loggingMiddleware } from './middleware/logging-middleware'
+import { sessionMiddleware } from './middleware/session-middleware'
 
 const app = express() //Creates an instance of express
 
 app.use(bodyparser.json()) //Turns JSON string into a JS Object
 
-app.use('/login')
-app.use('users')
-app.use('/users/:id')
-app.use('/reimbursements/status/:statusId')
-app.use('/reimbursements/author/userId/:userId')
-app.use('/reimbursements')
+app.use(loggingMiddleware) //Middleware for logging
+
+app.use(sessionMiddleware) //Adds a session object to every req object. Access using req.session
+
+//Available Endpoints
+app.use('/login') //Login
+app.use('/users') //Find Users, Update User, Update Reimbursement
+app.use('/users/:id') //Find Users By ID
+app.use('/reimbursements/status/:statusId') //Find Reimbursement by Status
+app.use('/reimbursements/author/userId/:userId') //Find reimbursements by user
+app.use('/reimbursements') //submit reimbursement
 
 
 const PORT = 1001
