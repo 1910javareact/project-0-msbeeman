4780d49b13ad02444405d1890043e01f0af50427
Matthew <msbeeman@github.com>
2019-11-21 12:54:28 -0500

    installed express-session, installed Dev  @types/express-session. Finished adding constructors to models

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,13 @@ const app = express() //Creates an instance of express
 
 app.use(bodyparser.json()) //Turns JSON string into a JS Object
 
+app.use('/login')
+app.use('users')
+app.use('/users/:id')
+app.use('/reimbursements/status/:statusId')
+app.use('/reimbursements/author/userId/:userId')
+app.use('/reimbursements')
+
+
 const PORT = 1001
 app.listen(PORT || 1001, () => console.log(`The server has started on port: ${PORT}`));
